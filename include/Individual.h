5185e1f0f9635a3799aa8b1982688ebf8c8cdb16
Adam Kowalski <kowalskiadam67@gmail.com>
2026-03-24 21:28:37 +0100

    Fixed Individual.h issues

---
+++ include/Individual.h
@@ -0,0 +1,14 @@
+#ifndef INDIVIDUAL_H
+#define INDIVIDUAL_H
+struct Individual{
+    std::vector<int> schedule;
+
+    int fitness;
+
+    Individual(const std::vector<int>& s, int f) : schedule(s), fitness(f) {}
+
+    bool operator<(const Individual& other) const {
+        return fitness < other.fitness;
+    }
+};
+#endif //INDIVIDUAL_H
