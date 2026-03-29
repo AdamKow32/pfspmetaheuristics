e3277a02fe504aa7c9a5c35e1d5949852ca00496
Adam Kowalski <kowalskiadam67@gmail.com>
2026-03-30 00:00:33 +0200

    Fixed Individual.h issues

--- include/Individual.h
+++ include/Individual.h
@@ -1,14 +1,20 @@
 #ifndef INDIVIDUAL_H
 #define INDIVIDUAL_H
+#include <vector>
+#include <ostream>
 struct Individual{
     std::vector<int> schedule;
 
     int fitness;
 
+    Individual();
     Individual(const std::vector<int>& s, int f) : schedule(s), fitness(f) {}
 
     bool operator<(const Individual& other) const {
         return fitness < other.fitness;
     }
+
+    bool hasFitness() const;
+    void print(std::ostream& os) const;
 };
 #endif //INDIVIDUAL_H
