692702ed5fefe2c7aa196dff0c8543666cffc45f
kowal <kowalskiadam67@gmail.com>
2026-04-02 18:21:11 +0200

    EA.h added with comments for each segments

--- include/EvolutionaryAlgorithm.h
+++ include/EvolutionaryAlgorithm.h
@@ -1,20 +1,74 @@
 #ifndef EVOLUTIONARYALGORITHM_H
 #define EVOLUTIONARYALGORITHM_H
 #include <vector>
+#include <string>
 #include <random>
+#include <unordered_map>
 #include "Individual.h"
 #include "ProblemInstance.h"
+#include "Evaluator.h"
 class EvolutionaryAlgorithm {
+public:
+    struct Config{
+        int    popSize        = 100;
+        int    generations    = 100;
+        double px             = 0.7;
+        double pm             = 0.1;
+        int    tournamentSize = 5;
+        int    eliteCount     = 2;
+        std::string mutationType  = "inversion";
+        std::string crossoverType = "ox";
+        std::string selectionType = "tournament";
+        std::string initType      = "random";
+        int    seed           = -1;
+        std::string logFile   = "resultsEA.csv";
+    };
+
+    struct GenerationStatistics {
+        int    generation;
+        int    best;
+        double avg;
+        int    worst;
+    };
+
+
+    EvolutionaryAlgorithm(const ProblemInstance& inst, Evaluator& evaluator, const Config& config);
 private:
     const ProblemInstance& instance;
-    std::mt19937 rng;
+    Evaluator& evaluator_;
+    const Config& config_;
+    mutable std::mt19937 random;
 
-public:
-    EvolutionaryAlgorithm(const ProblemInstance& inst);
+    std::vector<Individual> population;
+    Individual bestEver;
+    std::vector<GenerationStatistics> history_;
 
-    Individual createRandomIndividual;
-
+    // Initialization
+    Individual createRandomIndividual() const;
     std::vector<Individual> initializePopulation(int size);
+    void evaluateAll(std::vector<Individual>& population);
 
+    // Selection
+    const Individual& tournament() const;
+    const Individual& roulette() const;
+    const Individual& selectOne() const;
+    std::vector<Individual> elite(int count) const;
+
+    // Crossover
+    static std::pair<int, int> randomSegment(int J, std::mt19937& rng);
+    Individual ox(const Individual& p1, const Individual& p2) const;
+    Individual pmx(const Individual& p1, const Individual& p2) const;
+    Individual applyCrossover(const Individual& p1, const Individual& p2);
+
+    // Mutation
+    Individual swapMutation(Individual ind) const;
+    Individual inversionMutation(Individual ind) const;
+    Individual applyMutation(Individual ind) const;
+
+    // Help functions
+    GenerationStatistics computeStatistics(int gen) const;
+    void updateBest(const Individual& ind);
+    void saveLogs() const;
 };
+
 #endif //EVOLUTIONARYALGORITHM_H
